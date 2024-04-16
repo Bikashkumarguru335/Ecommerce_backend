4ba8db17abfb9f22c5a95bcc4ae44d51c6474ca8
Bikashkumarguru335 <167055739+Bikashkumarguru335@users.noreply.github.com>
2024-04-15 22:25:22 -0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 const mongoose=require("mongoose")
-DB_URI="mongodb://localhost:27017/Ecommerce";
+
 
 const connectDatabase=()=>{
 
-mongoose.connect(DB_URI)
+mongoose.connect(process.env.DB_URI)
 .then((data)=>console.log(`database is connected:${data.connection.host}`))
 .catch((err)=>console.log(err))
 }
