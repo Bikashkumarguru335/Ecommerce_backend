c7cf273a2ca63219f6f7a44b9b1f6dffd0765077
Bikashkumarguru335 <167055739+Bikashkumarguru335@users.noreply.github.com>
2024-05-08 23:38:32 -0700

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ cloudinary.config(
         api_secret:process.env.CLOUDINARY_API_SECRET
     }
 )
-const PORT = process.env.PORT || 4007; 
+const PORT = process.env.PORT; 
 
  const server=app.listen(PORT,()=>{
     console.log(`server is working on http://localhost:${PORT}`)
