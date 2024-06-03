ab0f5209ef6709e04cc539045832055545e2a39e
Bikashkumarguru335 <167055739+Bikashkumarguru335@users.noreply.github.com>
2024-06-02 21:17:43 -0700

    Update userControler.js

--- controler/userControler.js
+++ controler/userControler.js
@@ -117,7 +117,7 @@ exports.resetPassword=catchAsyncErr(async(req,res,next)=>{
 })
 //get user details 
 exports.getuserDetails=catchAsyncErr(async(req,res,next)=>{
-    const user=await User.findById(req.user.id);
+    const user=await User.findById(req.user);
     sendToken(user,200,res)
 })
 //update user password
