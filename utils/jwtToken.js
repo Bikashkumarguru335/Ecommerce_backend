3ea8d0c1ee20958f32eb43de1f51c475eb0d847d
Bikashkumarguru335 <167055739+Bikashkumarguru335@users.noreply.github.com>
2024-06-18 08:37:18 -0700

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -5,7 +5,7 @@ const token=user.getJwtToken()
 //option for cookie
       const options={
             expires:new Date(
-               Date.now() + process.env.COOKIE_EXPIRE*24*60*60*10),
+               Date.now() + process.env.COOKIE_EXPIRE*24*60*60*1000),
                httpOnly:true
 };
     // const options= {httpOnly:true};
