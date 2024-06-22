bef1f99c26b481aa6bd8a9c33e293e859366ff1c
Bikashkumarguru335 <167055739+Bikashkumarguru335@users.noreply.github.com>
2024-06-21 22:21:11 -0700

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,15 +1,16 @@
 //creating token and saving in cookie
 const sendToken=(user,statusCode,res)=>{
 const token=user.getJwtToken()
-     
+  console.log(token);   
 //option for cookie
       const options={
             expires:new Date(
                Date.now() + process.env.COOKIE_EXPIRE*24*60*60*1000),
                httpOnly:true,
            secure: process.env.NODE_ENV === 'production', 
+               sameSite: 'None'
+
 };
-    // const options= {httpOnly:true};
              
      res.status(statusCode).cookie('token',token,options).json({
          sucess:true,
