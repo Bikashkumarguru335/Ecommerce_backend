d050ca044a6cf957597080d35e6abae0b51e3080
Bikashkumarguru335 <167055739+Bikashkumarguru335@users.noreply.github.com>
2024-06-23 10:30:00 -0700

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -5,16 +5,17 @@ const token=user.getJwtToken()
   console.log("jwt folder",token);   
   }
 //option for cookie
-      const options={
-            expires:new Date(
-               Date.now() + process.env.COOKIE_EXPIRE*24*60*60*1000),
-               // httpOnly:true,
+      // const options={
+      //       expires:new Date(
+      //          Date.now() + process.env.COOKIE_EXPIRE*24*60*60*1000),
+      //          // httpOnly:true,
            // secure: process.env.NODE_ENV === 'production', 
            //     sameSite: 'None'
 
 };
              
-     res.cookie('token',token,options).status(statusCode).json({
+     // res.cookie('token',token,options).
+       res.status(statusCode).json({
          sucess:true,
          user,
          token,
