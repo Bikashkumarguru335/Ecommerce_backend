7457e17839479bb36aa88f278f267068d64ffad4
Bikashkumarguru335 <167055739+Bikashkumarguru335@users.noreply.github.com>
2024-06-23 08:20:49 -0700

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -9,6 +9,7 @@ const router=express.Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+router.route("/protected").get(isAuthenticationUser,)
 router.route("/password/forgot").post(forgotPassword)
 router.route("/password/reset/:token").put(resetPassword)
 router.route("/logout").get(logout);
