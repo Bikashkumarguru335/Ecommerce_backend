f5f5f2a50633d307273f3b063cc2ddc01a6538ff
Bikashkumarguru335 <167055739+Bikashkumarguru335@users.noreply.github.com>
2024-06-24 08:23:05 -0700

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -24,7 +24,7 @@ exports.isAuthenticationUser=catchAsyncErr(async(req,res,next)=>{
 
   next();
 });
- exports.authorizeRoles=(...roles)=>{
+ exports.authorizeRoles=(...role)=>{
      return (req,res,next)=>{
          if(!roles.includes(user.role)){
              console.log("authorization",user.role)
